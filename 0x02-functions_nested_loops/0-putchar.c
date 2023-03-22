fcf0150a6f195343d8dc648fb0418875b8e8ac9f
ibrahim7769 <ibrahim.hammani@usmba.ac.ma>
2023-03-22 04:20:10 -0700

    alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -19,3 +19,15 @@ int main(void)
 
     return (0);
 }
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (putchar(c));
+}
