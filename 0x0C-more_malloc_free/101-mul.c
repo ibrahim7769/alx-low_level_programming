34c943c4715d3b1ec06cee799e3fbdfb246bebfa
ibrahim7769 <ibrahim.hammani@usmba.ac.ma>
2023-04-13 18:05:56 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
-/**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
-{
-	int i;
+#include <stdlib.h>
+#include <ctype.h>
 
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
+int multiply(int num1, int num2) {
+    return num1 * num2;
 }
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
-{
-	int i, result;
 
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
-}
-/**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
-int main(int argc, char *argv[])
-{
-	int i;
-
-	malloc();
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (_isdigit(argv[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-	return (0);
+int main(int argc, char *argv[]) {
+    // Check for correct number of arguments
+    if (argc != 3) {
+        printf("Error\n");
+        return 98;
+    }
+    // Check if arguments are composed of digits
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 98;
+            }
+        }
+    }
+    // Parse arguments
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    // Perform multiplication
+    int result = multiply(num1, num2);
+    // Print result
+    printf("%d\n", result);
+    return 0;
 }
